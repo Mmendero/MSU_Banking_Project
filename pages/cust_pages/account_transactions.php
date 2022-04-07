bbb70c1e118b83df866a0622d6074e3850937541
Matthew <matthewmendero@gmail.com>
2022-04-07 12:43:46 -0400

    Updated Website to Bootstrap 5

--- pages/cust_pages/account_transactions.php
+++ pages/cust_pages/account_transactions.php
@@ -31,6 +31,8 @@
       integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
       crossorigin="anonymous"
     />
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../../styles/styles.css"/>
 
     <!-- FontAwesome Icons -->
@@ -45,61 +47,54 @@
   <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="customer_homepage.php">Accounts</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <div class="container-fluid">
+        <a class="navbar-brand" href="customer_homepage.php">Admin Frontpage</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="withdraw.php#">Withdraw</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="withdraw.php#">Withdraw</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="deposit.php#">Deposit</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="deposit.php#">Deposit</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="transfer.php#">Transfer Money</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="transfer.php#">Transfer Money</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="customer_manage.php">Manage Account</a>
+            </li>
+          </ul>
 
-          <li class="nav-item">
-            <a class="nav-link" href="customer_manage.php">Manage Account</a>
-          </li>
-        </ul>
-
-        <form class="form-inline my-2 my-lg-0" action="" method="POST">
-          <button name="logout" class="btn btn-info form-inline my-2 my-lg-0">
-            Logout
-          </button>
-        </form>
-        
+          <form class="d-flex form-inline my-2 my-lg-0" action="" method="POST">
+            <button name="logout" class="btn btn-info form-inline my-2 my-lg-0">
+              Logout
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
-    
-    <!-- Homepage Greeting -->
-    <?php
-      // Get time info.
-      date_default_timezone_set('America/New_York');
-      $date = date('F, d Y',strtotime(date("Y-m-d")));
-      if(date("a") == 'pm'){
-        $time = 'evening';
-      }
-      else{
-        $time = 'morning';
-      }
-      
-      // Get user info.
-      $query = "SELECT * FROM CUSTOMER WHERE ID = '".$_SESSION['user_id']."'";
-      $user = $db->query($query)->fetch_assoc();
 
-      echo '<div class="homepage-greeting">';
-      echo '<h1>Good '.$time.' '.$user['fname'].'!</h1>';
-      echo '<h6>Today\'s date is '.$date.'</h7>';
-      echo '</div>';
-    ?>
+    <div class="accounts">
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
+
+    <!-- Total Account Balance -->
+    <div class="balance">
+        <h7>Available Balance</h7>
+        <h1 class="balance-amount">$100.00</h1>
+    </div>
 
     <!-- Account Listing -->
     <div class="account_table">
@@ -107,33 +102,39 @@
       <table class="table table-hover table-bordered">
         <thead>
           <tr>
-            <th scope="col">Account</th>
-            <th scope="col">Available Balance</th>
-            <th scope="col">Pending Transactions</th>
+            <th scope="col">Date</th>
+            <th scope="col">Type</th>
+            <th scope="col">Description</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Balance</th>
           </tr>
         </thead>
         <?php
-          $query = "SELECT * FROM ACCOUNT WHERE cust_id = '".$_SESSION['user_id']."'";
+          $query = "SELECT * FROM TRANSACTION WHERE acc_number = '".$_SESSION['POST']['acc_num']."'";
           $result = $db->query($query);
 
           $account_total = 0;
-          while ($row = $result->fetch_assoc()){
-            echo '<tbody>';
+
+          // Build Table of Transactions.
+          echo '<tbody>';
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()){
+              echo '<tr>';
+              echo '<td class="col-md-2">'.$row['type'].'</td>';
+              echo '<td class="col-md-2">$'.$row['date'].'</td>';
+              echo '<td class="col-md-4">$'.$row['type'].'</td>';
+              echo '<td class="col-md-2">$'.$row['name'].'</td>';
+              echo '<td class="col-md-2">$'.number_format($row['amount'], 2).'</td>';
+              echo '<td class="col-md-2">$'.number_format($row['balance'], 2).'</td>';
+              echo '</tr>';
+            }
+          }else{
             echo '<tr>';
-            echo '<td class="col-md-6">'.$row['type'].'</td>';
-            echo '<td>$'.number_format($row['balance'], 2).'</td>';
-            echo '<td>$'.number_format($row['pending'], 2).'</td>';
+            echo '<td class="no-transactions" colspan="7">No Transactions Available for this Account</td>';
             echo '</tr>';
-            
-            $account_total += $row['balance'];
           }
           echo '</tbody>';
-          echo '<tfoot>';
-          echo '<tr class="table-info">';
-          echo '<td class="col-md-6">Total</td>';
-          echo '<td colspan="2">$'.number_format($account_total, 2).'</td>';
-          echo '</tr>';
-          echo '</tfoot>';
+
         ?>
       </table>
 
--- pages/cust_pages/withdraw.php
+++ pages/cust_pages/withdraw.php
@@ -29,12 +29,7 @@
     />
 
     <!-- Bootstrap CSS -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
-      crossorigin="anonymous"
-    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../../styles/styles.css" />
 
     <!-- FontAwesome Icons -->
@@ -49,40 +44,37 @@
   <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="user_homepage.html#">Banking App</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <div class="container-fluid">
+        <a class="navbar-brand" href="customer_homepage.php">Admin Frontpage</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="customer_homepage.php#">Balance</a>
-          </li>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="withdraw.php#">Withdraw</a>
+            </li>
 
-          <li class="nav-item active">
-            <a class="nav-link" href="withdraw.php#">Withdraw</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="deposit.php#">Deposit</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="deposit.php#">Deposit</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="transfer.php#">Transfer Money</a>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="transfer.php#">Transfer Money</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="customer_manage.php">Manage Account</a>
+            </li>
+          </ul>
 
-          <li class="nav-item">
-            <a class="nav-link" href="user_account.php#">Manage Account</a>
-          </li>
-        </ul>
-
-        <form class="form-inline my-2 my-lg-0" action="" method="POST">
-          <button name="logout" class="btn btn-info form-inline my-2 my-lg-0">
-            Logout
-          </button>
-        </form>
-        
+          <form class="d-flex form-inline my-2 my-lg-0" action="" method="POST">
+            <button name="logout" class="btn btn-info form-inline my-2 my-lg-0">
+              Logout
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
 
