8312c1a7392964343bf3cec3f44559c3375a37c4
Brianna <briesarmiento@gmail.com>
2022-04-08 17:39:54 -0400

    Updated customer manage page

--- pages/cust_pages/customer_manage.php
+++ pages/cust_pages/customer_manage.php
@@ -1,6 +1,6 @@
 <?php
 include '../../config.php';
-session_start();
+
 //gets session info
 session_start();
 
@@ -9,6 +9,12 @@ if (isset($_POST["logout"])) {
   $_SESSION['loggedin'] = false;
   header('Location: ../cust_pages/customer_signin.php');
 }
+// Validate Login.
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == false) {
+  $_SESSION['loggedin'] = false;
+  header('Location: ../cust_pages/customer_signin.php');
+}
+
 //creates query to get user info from CUSTOMER view
 $query = "SELECT * FROM CUSTOMER WHERE username = '" . $_SESSION['user'] . "'";
 
@@ -29,63 +35,58 @@ $db->close();
 ?>
 
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../styles/styles.css"/>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <!-- FontAwesome Icons -->
-    <script
-      src="https://kit.fontawesome.com/2727c3ff62.js"
-      crossorigin="anonymous"
-    ></script>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../styles/styles.css" />
 
-    <title>Banking App</title>
-  </head>
+  <!-- FontAwesome Icons -->
+  <script src="https://kit.fontawesome.com/2727c3ff62.js" crossorigin="anonymous"></script>
 
-  <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="customer_homepage.php">Accounts</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+  <title>Banking App</title>
+</head>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="withdraw.php#">Withdraw</a>
-            </li>
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="customer_homepage.php">Accounts</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-            <li class="nav-item">
-              <a class="nav-link" href="deposit.php#">Deposit</a>
-            </li>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="withdraw.php#">Withdraw</a>
+          </li>
 
-            <li class="nav-item">
-              <a class="nav-link" href="transfer.php#">Transfer Money</a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="deposit.php#">Deposit</a>
+          </li>
 
-            <li class="nav-item">
-              <a class="nav-link" href="customer_manage.php">Manage Account</a>
-            </li>
-          </ul>
+          <li class="nav-item">
+            <a class="nav-link" href="transfer.php#">Transfer Money</a>
+          </li>
 
-          <form class="d-flex form-inline my-2 my-lg-0" action="" method="POST">
-            <button name="logout" class="btn btn-info form-inline my-2 my-lg-0">
-              Logout
-            </button>
-          </form>
-        </div>
+          <li class="nav-item">
+            <a class="nav-link" href="customer_manage.php">Manage Account</a>
+          </li>
+        </ul>
+
+        <form class="d-flex form-inline my-2 my-lg-0" action="" method="POST">
+          <button name="logout" class="btn btn-info form-inline my-2 my-lg-0">
+            Logout
+          </button>
+        </form>
       </div>
-    </nav>
+    </div>
+  </nav>
 
   <h1>Profile and Settings</h1>
   <div>
@@ -118,4 +119,5 @@ $db->close();
     </center>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
