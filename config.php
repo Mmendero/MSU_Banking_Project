d10735ce41d4c2d6af2027d010385ec79723c597
Brianna <briesarmiento@gmail.com>
2022-04-11 18:08:35 -0400

    Fixed database connection

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 
     // Creates new database object
     // --Update with your own local DB creds--
-    $db = new mysqli('localhost', 'G6_admin', 'CSIT415OBS', 'group6_banking');
+    $db = new mysqli('localhost', 'menderm1_G6_Admin', 'CSIT415OBS', 'menderm1_MSU_Banking_App');    
     // Checks connection to database
     if (mysqli_connect_errno()) {
         echo 'Error: could not connect to database. Please try again later.';
