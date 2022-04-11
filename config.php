261e88547120e8a1924ae384a5a7e0c577bfbd0b
Brianna <briesarmiento@gmail.com>
2022-04-11 17:40:47 -0400

    Merge branch 'main' of https://github.com/Mmendero/MSU_Banking_Project

--- config.php
+++ config.php
@@ -4,8 +4,7 @@
 
     // Creates new database object
     // --Update with your own local DB creds--
-    $db = new mysqli('localhost', 'menderm1_G6_Admin', 'CSIT415OBS', 'menderm1_MSU_Banking_App');
-    
+    $db = new mysqli('localhost', 'menderm1_G6_Admin', 'CSIT415OBS', 'menderm1_MSU_Banking_App');    
     // Checks connection to database
     if (mysqli_connect_errno()) {
         echo 'Error: could not connect to database. Please try again later.';
